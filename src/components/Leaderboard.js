978b90787aa5333330fd060b597315f6bcae3252
MichalKukielka <kukielka.michal@gmail.com>
2019-11-03 18:27:52 +0100

    Leaderboard view scaffold

---
+++ src/components/Leaderboard.js
@@ -0,0 +1,24 @@
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import {
+    Segment,
+    Header,
+    Grid,
+    Image,
+    Label,
+    Divider,
+} from 'semantic-ui-react';
+
+export class Leaderboard extends Component {
+
+    render () {
+        return (
+            <Fragment>
+
+            </Fragment>
+        )
+    }
+}
+
+export default Leaderboard
